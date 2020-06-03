217ca98d0ded2c5ead58b3c9e102828bd5acde9d
batuhangunday <batuhan.gunday@icloud.com>
2020-06-03 22:31:41 +0300

    Initial Commit

---
+++ bungeeapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bungeeapp
+//
+//  Created by Batuhan Günday on 3.06.2020.
+//  Copyright © 2020 Bungee Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
